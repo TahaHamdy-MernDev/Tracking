525ebce1bffda2c48378043ee26d484ad0bcb97d
TahaHamdy-MernDev <TahaHamdy.mern@gmail.com>
2024-05-17 11:34:15 +0300

    ...

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,7 +5,8 @@ import { CookiesProvider } from 'react-cookie';
 export default function App() {
   return (
     <div className="box-border p-0 m-0 bg-gradient-to-b from-[#020B27] to-blue-500">
-      <HashRouter basename="/Attendance-Tracking-Client" >
+      {/* <HashRouter basename="/Attendance-Tracking-Client" > */}
+      <HashRouter>
       <CookiesProvider defaultSetOptions={{ path: 'Attendance-Tracking-Client/' }}>
       <AuthProvider>
         <ProjectRoutes />
